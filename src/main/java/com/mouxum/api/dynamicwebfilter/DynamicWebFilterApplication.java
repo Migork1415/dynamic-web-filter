f25c7a4766d3cd8c60cc8a57436e00afc2592102
m1igu <Sporting69>
2018-12-10 12:01:17 +0000

    Initial commit

---
+++ src/main/java/com/mouxum/api/dynamicwebfilter/DynamicWebFilterApplication.java
@@ -0,0 +1,12 @@
+package com.mouxum.api.dynamicwebfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicWebFilterApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( DynamicWebFilterApplication.class, args );
+	}
+}
